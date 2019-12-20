86279ad55d5f421538c69645effb0158fe52a287
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-19 19:13:49 -0500

    finished first iteration of main page

--- src/components/learning.tsx
+++ src/components/learning.tsx
@@ -26,64 +26,101 @@ const Learning:React.FC = (props: object) => {
             img: 'https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'GO 101',
+            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZbFOW6r6YulR1_9SCrdyOZ',
+            img: 'https://sdtimes.com/wp-content/uploads/2018/02/golang.sh_-490x490.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'Clojure 101',
+            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbarM5X9VpYCVtPH85rsunv0',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Clojure_logo.svg/1024px-Clojure_logo.svg.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'CS Concepts',
+            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbbfwjU1ToZlUWCinxmFqlIp',
+            img: 'https://www.mpsd.org/cms/lib/MA02212716/Centricity/Domain/99/Computer%20Science%20Banner%20Small.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'Dart 101',
+            url: 'https://www.youtube.com/watch?v=BTn36FhprnM&list=PLY6oTPmKnKbZaOObALdHSgGTZhne6Q-yJ',
+            img: 'https://www.stickpng.com/assets/images/5847f289cef1014c0b5e486b.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'Elixir 101',
+            url: 'https://www.youtube.com/watch?v=MQYHu2GuRfU&list=PLY6oTPmKnKbYZhj8LsQ3yq1vn-HwlpkkS',
+            img: 'https://www.columbuselixir.com/images/elixir-logo.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'Swift 101',
+            url: 'https://www.youtube.com/watch?v=ctughz7bbnQ&list=PLY6oTPmKnKbYn60pD3UYfYw7BzxGY2Do0',
+            img: 'http://static1.squarespace.com/static/558def25e4b0fc259f066636/5a49786bf9619ae3bb6a2564/59307f9b15cf7dadce1c0bfa/1564669693374/Swift_logo.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'Kotlin 101',
+            url: 'https://www.youtube.com/watch?v=N5z2RDR9sk0&list=PLY6oTPmKnKba9Xg06DR67IxdppdKsQZ_s',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Kotlin-logo.svg/1024px-Kotlin-logo.svg.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'Ruby 101',
+            url: 'https://www.youtube.com/watch?v=4yMGIgXcJJM&list=PLY6oTPmKnKbZp8Kh6jS5A6j-6H2kGY12e',
+            img: 'https://blog.mwpreston.net/wp-content/uploads/2018/09/ruby-logo.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'Full Stack with KoaJS',
+            url: 'https://www.youtube.com/watch?v=8_aWw7lfKKI&list=PLY6oTPmKnKbbF4t0Y9DcUVYi7f4kix7Qj',
+            img: 'https://seeklogo.com/images/K/koa-logo-D494764315-seeklogo.com.png'
         },
         {
-            topic: 'Javascript 101',
-            url: 'https://www.youtube.com/playlist?list=PLY6oTPmKnKbZDZ9cRrRby4Wnr4GIJj5O3',
-            img: 'https://cdn.worldvectorlogo.com/logos/javascript.svg'
+            topic: 'StencilJS',
+            url: 'https://www.youtube.com/watch?v=btQ7Whq8xcU&list=PLY6oTPmKnKbazpUTMcGmvMtgU5sr0Ip-V',
+            img: 'https://www.javascripttuts.com/images/technologies/Stencil.png'
+        },
+        {
+            topic: 'SvelteJS',
+            url: 'https://www.youtube.com/watch?v=Cu-JVd8MKaU&list=PLY6oTPmKnKbZpyj6WhUsjri1Tw_BO-obP',
+            img: 'https://pbs.twimg.com/profile_images/1121395911849062400/7exmJEg4_400x400.png'
+        },
+        {
+            topic: 'VueJS',
+            url: 'https://www.youtube.com/watch?v=_EwtLQ7BZkc&list=PLY6oTPmKnKbbsEAIDfFAlhAVbSCIt2Bxx',
+            img: 'https://vuejs.org/images/logo.png'
+        },
+        {
+            topic: 'Angular',
+            url: 'https://www.youtube.com/watch?v=_EwtLQ7BZkc&list=PLY6oTPmKnKbahNK_YUsjTzP5U-FkGA544',
+            img: 'https://angular.io/assets/images/logos/angular/angular.png'
+        },
+        {
+            topic: 'Full Stack with ExpressJS',
+            url: 'https://www.youtube.com/watch?v=SZxyYKhAvQQ&list=PLY6oTPmKnKbZeKlNGxOrL2Crl-tm1bxbJ',
+            img: 'https://bs-uploads.toptal.io/blackfish-uploads/skill_page/content/logo_file/logo/5982/express_js-161052138fa79136c0474521906b55e2.png'
+        },
+        {
+            topic: 'PHP 101',
+            url: 'https://www.youtube.com/watch?v=scsmssXd47U&list=PLY6oTPmKnKbbLnwRkrCDhOl94iaibHwBJ',
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/711px-PHP-logo.svg.png'
+        },
+        {
+            topic: 'Full Stack with FoalTS',
+            url: 'https://www.youtube.com/watch?v=mvoBeTdMFUE&list=PLY6oTPmKnKbY6v7zR9h1INfywh-qFLSQ_',
+            img: 'https://foalts.org/assets/img/logo_400.png'
+        },
+        {
+            topic: 'React',
+            url: 'https://www.youtube.com/watch?v=SZxyYKhAvQQ&list=PLY6oTPmKnKbZeKlNGxOrL2Crl-tm1bxbJ',
+            img: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'
         }
     ]
 
     return (
+        <>
         <div className="learning">
         {playlists.map(topic => <Topic topic={topic}/>)}
 
-        </div>
-    )
 
+        </div>
+        <a href="https://www.youtube.com/channel/UCoc4UCEetAt3htM3hV1dQgQ/playlists"><h3>Find More Coding Video Playlists Here</h3></a>
+        </>)
 }
 
 export default Learning;
--- src/components/tech.tsx
+++ src/components/tech.tsx
@@ -3,9 +3,40 @@ import './tech.css';
 
 const Tech: React.FC = (props: object) => {
 
+const techData = [
+    {
+        type: "Languages",
+        includes: ['Javascript','Typescript','Python', 'Ruby', 'C', 'C#', 'C++','GO', 'Java', 'Objective-C', 'Swift', 'Kotlin', 'Dart', 'Elixir', 'Scala', 'Lisp', 'Clojure', 'ELM', 'Haskell', 'OCAML', 'Web Assembly', 'Prolog', 'Crystal', 'Ballerina', 'Rust', 'Haxe', 'Groovy', 'F#', 'VB.NET']
+    },
+    {
+        type: "Frontend Frameworks",
+        includes: ['React', 'Angular', 'Vue', 'Svelte', 'Stencil', 'Ember']
+    },
+    {
+        type: "Backend Frameworks",
+        includes: ['Express(JS)', 'Koa(JS)', 'FoalTS(JS/TS)', 'Adonis(JS)', 'Next(JS)', 'Nuxt(JS)', 'Nest(JS)', 'Django(Python)', 'Flask(Python)', 'Bottle(Python)', 'Buffalo(GO)', 'Spring(Java)', 'Laravel(PHP)', 'Symphony(PHP)', 'CodeIgniter(PHP)', '.Net(C#,F#,VB.net)', 'Ktor(Kotlin)', 'Vapor(Swift)', 'Rocket(Rust)', 'Rails(Ruby)', 'Sinatra(Ruby)']
+    },
+    {
+        type: "Databases",
+        includes: ['MySQL', 'PostgreSQL', 'MongoDB','DynamoDB','MariaDB','Scylla', 'SQLite']
+    },
+    {
+        type: "Cloud Providers",
+        includes: ['AWS', 'Azure', 'gCloud']
+    }
+]
 
 return (
-    <div>
+    <div className="tech">
+        {techData.map(tech => (
+            <div className="type">
+            <h1>{tech.type}</h1>
+            <ul>
+            {tech.includes.map(item => <li>{item}</li>)}
+            </ul>
+            </div>
+        ))}
+
 
     </div>
 )
