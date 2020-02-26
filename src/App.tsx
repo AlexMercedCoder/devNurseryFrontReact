5da86b8c612ed9dc64be57b93f104e159702357e
Alex Merced <alexmerced@alexmerced.com>
2020-02-26 14:29:45 -0500

    more playlists

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,54 @@
 import React from 'react';
 import './App.css';
-import Main from "./components/main"
-import Learning from "./components/learning"
-import Tech from "./components/tech"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Main from './components/main';
+import Learning from './components/learning';
+import Tech from './components/tech';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-    <Router>
-        <div className="logo"> <span>dev</span>Nursery </div>
+    return (
+        <div className="App">
+            <Router>
+                <div className="logo">
+                    {' '}
+                    <span>dev</span>Nursery{' '}
+                </div>
 
-        <nav>
-        <Link to="/">Home</Link>
-        <a href="https://mainblog.devNursery.com">Main Blog</a>
-        <a href="https://member.devNursery.com">Blogging</a>
-        <a href="http://blogs.devNursery.com">User Posts</a>
-        <Link to="/learning">Free Learning</Link>
-        <Link to="/tech">Technology List</Link>
-        <a href="https://join.slack.com/t/amwebdev/shared_invite/enQtNzc4NDA3MDU3MDE0LTZjNjIyZmQ3MzA5Y2Q3MWUwZjk3NTIyYjliOThlMWFjNTFkYWM1OGUxN2Y3NGExNGVhOGIzZTg0YTJjZTk5NDA">Slack Community</a>
-        </nav>
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/learning">Free Learning</Link>
+                    <Link to="/tech">Technology List</Link>
+                    <a href="https://join.slack.com/t/amwebdev/shared_invite/enQtNzc4NDA3MDU3MDE0LTZjNjIyZmQ3MzA5Y2Q3MWUwZjk3NTIyYjliOThlMWFjNTFkYWM1OGUxN2Y3NGExNGVhOGIzZTg0YTJjZTk5NDA">
+                        Slack Community
+                    </a>
+                    <a href="https://discord.gg/hQ2AySW">Discord Community</a>
+                    <a href="https://spectrum.chat/devnursery">
+                        Spectrum Community
+                    </a>
+                </nav>
 
-        <div className="container">
-        <Switch>
-        <Route exact path="/">
-        <Main/>
-        </Route>
-        <Route path="/learning">
-        <Learning/>
-        </Route>
-        <Route path="/tech">
-        <Tech/>
-        </Route>
-        </Switch>
+                <div className="container">
+                    <Switch>
+                        <Route exact path="/">
+                            <Main />
+                        </Route>
+                        <Route path="/learning">
+                            <Learning />
+                        </Route>
+                        <Route path="/tech">
+                            <Tech />
+                        </Route>
+                    </Switch>
+                </div>
+                <footer>
+                    Application created by Alex Merced of AlexMercedCoder.com,
+                    Created with ReactJS/Typescript
+                    <br />
+                    Contact Alex Merced to be part of your new project at
+                </footer>
+            </Router>
         </div>
-        <footer>
-        Application created by Alex Merced of AlexMercedCoder.com, Created with ReactJS/Typescript
-        </footer>
-    </Router>
-    </div>
-  );
-}
+    );
+};
 
 export default App;
--- src/components/tech.tsx
+++ src/components/tech.tsx
@@ -1,46 +1,105 @@
-import React from 'react';
-import './tech.css';
+import React from "react";
+import "./tech.css";
 
 const Tech: React.FC = (props: object) => {
-
-const techData = [
+  const techData = [
     {
-        type: "Languages",
-        includes: ['Javascript','Typescript','Python', 'Ruby', 'C', 'C#', 'C++','GO', 'Java', 'Objective-C', 'Swift', 'Kotlin', 'Dart', 'Elixir', 'Scala', 'Lisp', 'Clojure', 'ELM', 'Haskell', 'OCAML', 'Web Assembly', 'Prolog', 'Crystal', 'Ballerina', 'Rust', 'Haxe', 'Groovy', 'F#', 'VB.NET']
+      type: "Languages",
+      includes: [
+        "Javascript",
+        "Typescript",
+        "Python",
+        "Ruby",
+        "C",
+        "C#",
+        "C++",
+        "GO",
+        "Java",
+        "Objective-C",
+        "Swift",
+        "Kotlin",
+        "Dart",
+        "Elixir",
+        "Scala",
+        "Lisp",
+        "Clojure",
+        "ELM",
+        "Haskell",
+        "OCAML",
+        "Web Assembly",
+        "Prolog",
+        "Crystal",
+        "Ballerina",
+        "Rust",
+        "Haxe",
+        "Groovy",
+        "F#",
+        "VB.NET"
+      ]
     },
     {
-        type: "Frontend Frameworks",
-        includes: ['React', 'Angular', 'Vue', 'Svelte', 'Stencil', 'Ember']
+      type: "Frontend Frameworks",
+      includes: ["React", "Angular", "Vue", "Svelte", "Stencil", "Ember"]
     },
     {
-        type: "Backend Frameworks",
-        includes: ['Express(JS)', 'Koa(JS)', 'FoalTS(JS/TS)', 'Adonis(JS)', 'Next(JS)', 'Nuxt(JS)', 'Nest(JS)', 'Django(Python)', 'Flask(Python)', 'Bottle(Python)', 'Buffalo(GO)', 'Spring(Java)', 'Laravel(PHP)', 'Symphony(PHP)', 'CodeIgniter(PHP)', '.Net(C#,F#,VB.net)', 'Ktor(Kotlin)', 'Vapor(Swift)', 'Rocket(Rust)', 'Rails(Ruby)', 'Sinatra(Ruby)']
+      type: "Backend Frameworks",
+      includes: [
+        "Express(JS)",
+        "Koa(JS)",
+        "FoalTS(JS/TS)",
+        "Adonis(JS)",
+        "Next(JS)",
+        "Nuxt(JS)",
+        "Nest(JS)",
+        "Django(Python)",
+        "Flask(Python)",
+        "Bottle(Python)",
+        "Buffalo(GO)",
+        "Spring(Java)",
+        "Laravel(PHP)",
+        "Symphony(PHP)",
+        "CodeIgniter(PHP)",
+        ".Net(C#,F#,VB.net)",
+        "Ktor(Kotlin)",
+        "Vapor(Swift)",
+        "Rocket(Rust)",
+        "Rails(Ruby)",
+        "Sinatra(Ruby)",
+        "Apollo Server(JS)"
+      ]
     },
     {
-        type: "Databases",
-        includes: ['MySQL', 'PostgreSQL', 'MongoDB','DynamoDB','MariaDB','Scylla', 'SQLite']
+      type: "Databases",
+      includes: [
+        "MySQL",
+        "PostgreSQL",
+        "MongoDB",
+        "DynamoDB",
+        "MariaDB",
+        "Scylla",
+        "SQLite"
+      ]
     },
     {
-        type: "Cloud Providers",
-        includes: ['AWS', 'Azure', 'gCloud']
+      type: "Cloud Providers",
+      includes: ["AWS", "Azure", "gCloud"]
     }
-]
+  ];
 
-return (
+  return (
     <div className="tech">
-        {techData.map(tech => (
-            <div className="type">
-            <h1>{tech.type}</h1>
-            <ul>
-            {tech.includes.map(item => <li>{item}</li>)}
-            </ul>
-            </div>
-        ))}
-
-
+      {techData.map(tech => (
+        <div className="type">
+          <h1>{tech.type}</h1>
+          <ul>
+            {tech.includes.map(item => (
+              <li>{item}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
-)
-
-}
+  );
+};
 
 export default Tech;
