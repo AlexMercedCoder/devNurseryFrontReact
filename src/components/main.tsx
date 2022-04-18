37e15456d5047f505f5d096da3eccee3912c1b44
Alex Merced <alex@alexmerced.dev>
2022-04-18 16:06:18 -0400

    update

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -8,7 +8,7 @@ const Main: React.FC = (props: object) => {
         <div className="main">
             <h1>Welcome to DN!</h1>
             <p>Here at DN we hope to provide resources to help developers continue their growth. We do this by curating learning resources, fostering a community and creating a platform for developers to share that growth with the world. Welcome to devNursery, where we help grow developer careers, networks, and skills.</p>
-            <img src="https://vscode.readthedocs.io/en/latest/languages/images/typescript/typescript_hero.png"/>
+            <img src="/freeresources.png"/>
         </div>
     )
 
